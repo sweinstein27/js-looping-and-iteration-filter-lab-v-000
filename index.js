8c1212a334e47b6a598a191ff596f6027b5dcefc
Scott Weinstein <sweinstein27@gmail.com>
2018-10-22 18:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function findMatching(list, name){
   });
 }
 
-function fuzzyMatch(list, partialName) {
+function fuzzyMatch (list, partialName) {
   let lengthOfName = partialName.length;
-  return list.filer(function (drivername) {
-    return drivername.slice(0, lengthOfName) === partialName;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
   });
 }
