964fcfd7882bd2e7d5056a15ab57f130192e3a92
Scott Weinstein <sweinstein27@gmail.com>
2018-10-22 18:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching(list, name){
+  return list.filter(function (drivername){
+    return drivername.toLowerCase() === name.toLowerCase();
+  });
+}
